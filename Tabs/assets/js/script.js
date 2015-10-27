dbefa9b2baff49d43ec71d049985363359e7694f
Andy Tran <hello@andytran.me>
2015-10-26 19:25:16 -0700

    Added in tabs.

--- Tabs/assets/js/script.js
+++ Tabs/assets/js/script.js
@@ -7,4 +7,45 @@
 
 $(document).ready(function () {
 
+  // Tabs
+  $('.tabs-header a').on('click', function (e) {
+    e.preventDefault();
+
+    var tabId = $(this).attr('tab-id');
+
+    // Remove Active State
+    $('.tabs-header a').stop().parent().removeClass('active');
+
+    // Add Active State
+    $(this).stop().parent().addClass('active');
+
+    // Remove Active Class
+    $('.tab').stop().fadeOut(300, function () {
+      $(this).removeClass('active');
+    }).hide();
+
+    // Add Active Class
+    $('.tab[tab-id="' + tabId + '"]').stop().fadeIn(300, function () {
+      $(this).addClass('active');
+    }).show();
+  });
+
+  // Ripple
+  $('[ripple]').on('click', function (e) {
+    var rippleDiv = $('<div class="ripple" />'),
+      rippleOffset = $(this).offset(),
+      rippleY = e.pageY - rippleOffset.top,
+      rippleX = e.pageX - rippleOffset.left,
+      ripple = $('.ripple');
+
+    rippleDiv.css({
+      top: rippleY - (ripple.height() / 2),
+      left: rippleX - (ripple.width() / 2),
+      background: $(this).attr("ripple-color")
+    }).appendTo($(this));
+
+    window.setTimeout(function () {
+      rippleDiv.remove();
+    }, 1500);
+  });
 });
