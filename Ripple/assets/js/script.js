ed27b1293ee372a7ca647e69a2528f3a909af54f
Andy Tran <andyhqtran@gmail.com>
2015-11-09 17:33:43 -0800

    Added in ripple.

---
+++ Ripple/assets/js/script.js
@@ -0,0 +1,29 @@
+/*!
+ * Ripple
+ * Created by Andy Tran (http://andytran.me)
+ * Copyright 2015 Andy Tran.
+ * Licensed under MIT (https://github.com/andyhqtran/UILibrary/blob/master/LICENSE)
+ */
+
+$(document).ready(function () {
+
+  // Ripple
+  $('[ripple]').on('click', function (e) {
+    var rippleDiv = $('<div class="ripple" />'),
+      rippleOffset = $(this).offset(),
+      rippleY = e.pageY - rippleOffset.top,
+      rippleX = e.pageX - rippleOffset.left,
+      ripple = $('.ripple');
+
+    rippleDiv.css({
+      top: rippleY - (ripple.height() / 2),
+      left: rippleX - (ripple.width() / 2),
+      background: $(this).attr("ripple-color")
+    }).appendTo($(this));
+
+    window.setTimeout(function () {
+      rippleDiv.remove();
+    }, 1500);
+  });
+
+});
