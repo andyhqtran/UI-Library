fd2698114de98e8ecb27056a846a7a4026a203c2
Andy Tran <andyhqtran@gmail.com>
2015-11-09 17:26:02 -0800

    Added in elevation.

---
+++ Elevation/assets/js/script.js
@@ -0,0 +1,10 @@
+/*!
+ * Elevation Template
+ * Created by Andy Tran (http://andytran.me)
+ * Copyright 2015 Andy Tran.
+ * Licensed under MIT (https://github.com/andyhqtran/UILibrary/blob/master/LICENSE)
+ */
+
+$(document).ready(function () {
+
+});
