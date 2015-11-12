bd33a3fce872fdf309121a563e960a731ce5818f
Andy Tran <andyhqtran@gmail.com>
2015-11-12 14:40:53 -0800

    Fixed ripple effect bouncing around.

--- Ripple/assets/js/script.js
+++ Ripple/assets/js/script.js
@@ -10,20 +10,21 @@ $(document).ready(function () {
   // Ripple
   $('[ripple]').on('click', function (e) {
     var rippleDiv = $('<div class="ripple" />'),
+      rippleSize = 60,
       rippleOffset = $(this).offset(),
       rippleY = e.pageY - rippleOffset.top,
       rippleX = e.pageX - rippleOffset.left,
       ripple = $('.ripple');
 
     rippleDiv.css({
-      top: rippleY - (ripple.height() / 2),
-      left: rippleX - (ripple.width() / 2),
+      top: rippleY - (rippleSize / 2),
+      left: rippleX - (rippleSize / 2),
       background: $(this).attr("ripple-color")
     }).appendTo($(this));
 
     window.setTimeout(function () {
       rippleDiv.remove();
-    }, 1500);
+    }, 1900);
   });
 
 });
